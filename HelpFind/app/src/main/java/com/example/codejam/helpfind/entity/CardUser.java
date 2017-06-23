f3789bdc4db00d6bf6f2e2710c6f3e3a9c092719
Kornberg <kornbergfresnel@outlook.com>
2017-06-23 10:09:32 +0800

    user define

--- HelpFind/app/src/main/java/com/example/codejam/helpfind/entity/CardUser.java
+++ HelpFind/app/src/main/java/com/example/codejam/helpfind/entity/CardUser.java
@@ -7,10 +7,12 @@ package com.example.codejam.helpfind.entity;
 public class CardUser {
     private final String _userName;
     private final String _info;
+    private final String _imgURL;
 
-    public CardUser(String _userName, String _info) {
+    public CardUser(String _userName, String _info, String _imgURL) {
         this._userName = _userName;
         this._info = _info;
+        this._imgURL = _imgURL;
     }
 
     public String get_userName() {
@@ -19,5 +21,9 @@ public class CardUser {
 
     public String get_info() {
         return _info;
+    }
+
+    public String get_imgURL() {
+        return _imgURL;
     }
 }
