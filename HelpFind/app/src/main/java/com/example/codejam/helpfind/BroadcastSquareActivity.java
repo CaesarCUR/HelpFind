0025687f8af414be3c195cd96fc6f80814fed06d
Kornberg <kornbergfresnel@outlook.com>
2017-06-23 10:10:59 +0800

    add BroadcastSquareActivity

--- HelpFind/app/src/main/java/com/example/codejam/helpfind/BroadcastSquareActivity.java
+++ HelpFind/app/src/main/java/com/example/codejam/helpfind/BroadcastSquareActivity.java
@@ -4,13 +4,23 @@ import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.example.codejam.helpfind.entity.BroadcastAdapter;
+import com.example.codejam.helpfind.entity.CardUser;
+
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import it.gmariotti.recyclerview.adapter.AlphaAnimatorAdapter;
 
 /**
  * Created by zhouming on 2017/6/20.
@@ -23,6 +33,15 @@ public class BroadcastSquareActivity extends AppCompatActivity {
     @BindView(R.id.img_write) ImageView _broadText;
     @BindView(R.id.img_camera) ImageView _broadPhoto;
     @BindView(R.id.btn_open_map) Button _btnOpenMap;
+    @BindView(R.id.list_nearby) RecyclerView _listNearBy;
+
+    /**
+     * LayoutManager for RecyclerView should be implemented explicitly before it is used by
+     * a RecyclerView instant
+     */
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,6 +50,9 @@ public class BroadcastSquareActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setResult(RESULT_OK, null); // tell login activity: you can die
+
+        // initialization for RecyclerView
+        initial();
 
         _broadText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +74,30 @@ public class BroadcastSquareActivity extends AppCompatActivity {
                 // TODO: open map
             }
         });
+    }
+
+    private void initial() {
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        // TODO: initialize mAdapter
+        mAdapter = new BroadcastAdapter(getData(), getBaseContext());
+        // Set appearance animation for RecyclerView with reload data adapter
+        AlphaAnimatorAdapter animatorAdapter = new AlphaAnimatorAdapter(mAdapter, _listNearBy);
+        _listNearBy.setLayoutManager(mLayoutManager);
+        // Load wrapped adapter to RecyclerView
+        _listNearBy.setAdapter(animatorAdapter);
+//        _listNearBy.setItemAnimator();
+    }
+
+    private ArrayList<CardUser> getData() {
+        ArrayList<CardUser> data = new ArrayList<>();
+
+        for (int i = 0; i < 13; i++) {
+            String url = "file:///Users/zhouming/Desktop/catb.png";
+            data.add(new CardUser("Andrew", "My god! So handsome a genius!", url));
+        }
+
+        return data;
     }
 
 
